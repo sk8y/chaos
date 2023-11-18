e88168a5133a1629865c5b02bba374789d2d0bb0
gautsing <gautsing@cisco.com>
2023-11-18 15:36:04 -0800

    18-Nov-23-15:36:04 on main - wip

--- restart-pods-randomly.sh
+++ restart-pods-randomly.sh
@@ -5,12 +5,21 @@ set -euo pipefail
 
 max_pods_to_kill="${1:-5}"
 pods_selector_to_kill="${2:-test-app-hello}"
+kill_interval="${3:-10}"
 
 while true; do
-    for pod_name in $(kubectl -n default get pods --selector "app=${pods_selector_to_kill}" --no-headers -o=custom-columns='NAME:.metadata.name' | tail -"${max_pods_to_kill}"); do
-        echo "Deleting pod ${pod_name}"
+
+    for pod_name in $(kubectl -n default get pods \
+            --selector "app=${pods_selector_to_kill}" \
+            -o custom-columns=POD:metadata.name,READY-true:status.containerStatuses[*].ready \
+            --no-headers \
+                | grep true \
+                | awk '{print $1}' \
+                | shuf -n "${max_pods_to_kill}"
+            ); do
+        echo "$(date +%y%m%d-%H%M%S) - deleting pod ${pod_name}"
         kubectl -n default delete pod "${pod_name}"
-        sleep 1
+        sleep 0.1
     done
-    sleep 10
+    sleep "${kill_interval}"
 done
